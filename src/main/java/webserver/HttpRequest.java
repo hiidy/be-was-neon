1d0ce21a63fa6e768451f0b530559c5e56d715b2
hiidy <hiidy115@gmail.com>
2024-03-12 13:45:39 +0900

    HttpRequest클래스를 수정해서 쿼리 매개변수를 처리하도록 변경
    - 한글로 닉네임을 지으면 인코딩 되기 때문에 URLDecoder를 이용해서 nickname을 디코딩 해줬습니다.
    - query 파싱 과정은 우선 &로 각각의 정보를 나누고 =로 key value를 나눠줬습니다.
    - query정보에 접근할 경우 불변객체를 통해 정보를 얻도록 했습니다.

--- src/main/java/webserver/HttpRequest.java
+++ src/main/java/webserver/HttpRequest.java
@@ -1,16 +1,26 @@
 package webserver;
 
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public class HttpRequest {
 
     private String method;
     private String requestURI;
 
+    private final Map<String, String> queries = new HashMap<>();
 
     public HttpRequest(String requestLine) {
         String[] requestLines = requestLine.split(" ");
         method = requestLines[0];
-        requestURI = requestLines[1];
-
+        if (hasQueryParameter(requestLines[1])) {
+            extractQueryFromURI(requestLines[1]);
+        } else {
+            requestURI = requestLines[1];
+        }
     }
 
     public String getMethod() {
@@ -20,4 +30,36 @@ public class HttpRequest {
     public String getRequestURI() {
         return requestURI;
     }
+
+    private boolean hasQueryParameter(String requestLine) {
+        return requestLine.contains("?");
+    }
+
+    private void extractQueryFromURI(String URI) {
+        String[] URIs = URI.split("\\?");
+        requestURI = URIs[0];
+        queries = parseQueryString(URIs[1]);
+    }
+
+    private Map<String, String> parseQueryString(String string) {
+        Map<String, String> queryMap = new HashMap<>();
+
+        String[] pairs = string.split("&");
+
+        for (String pair : pairs) {
+
+            String[] keyValue = pair.split("=");
+
+            String key = keyValue[0];
+            String value = URLDecoder.decode(keyValue[1], StandardCharsets.UTF_8);
+
+            queryMap.put(key, value);
+        }
+        return queryMap;
+    }
+
+    public Map<String, String> getQueries() {
+        return Collections.unmodifiableMap(queries);
+    }
+
 }
