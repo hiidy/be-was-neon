19e4a854db213fbf9f521f898fc4ad1ca4e6b131
hiidy <hiidy115@gmail.com>
2024-03-19 13:42:29 +0900

    POST 메서드를 위해서 HTTP Request의 Body를 파싱하고 읽는 기능 추가

--- src/main/java/webserver/request/HttpRequest.java
+++ src/main/java/webserver/request/HttpRequest.java
@@ -10,16 +10,21 @@ public class HttpRequest {
     private static final Logger logger = LoggerFactory.getLogger(HttpRequest.class);
     private final HttpRequestStartLine httpRequestStartLine;
     private final HttpRequestHeader httpRequestHeader;
+    private HttpRequestBody httpRequestBody;
+    private static final String CRLF = "\r\n";
 
     public HttpRequest(BufferedReader httpRequest) throws IOException {
         httpRequestStartLine = new HttpRequestStartLine(readRequestLine(httpRequest));
         httpRequestHeader = new HttpRequestHeader(readHeaderLine(httpRequest));
+//        if (httpRequestStartLine.getMethod().equals("POST")) {
+//            httpRequestBody = new HttpRequestBody(readBodyLine(httpRequest));
+//        }
     }
 
     private String readRequestLine(BufferedReader httpRequest) throws IOException {
         StringBuilder requestBuilder = new StringBuilder();
         String line;
-        if ((line = httpRequest.readLine()) != null && !line.isEmpty()) {
+        if ((line = httpRequest.readLine()) != null && line != CRLF) {
             requestBuilder.append(line).append('\n');
             logger.debug("Request Line: {}", line);
         }
@@ -31,12 +36,22 @@ public class HttpRequest {
         StringBuilder requestHeaderBuilder = new StringBuilder();
         String line;
 
-        while ((line = httpRequest.readLine()) != null && !line.isEmpty()) {
+        while (!(line = httpRequest.readLine()).isEmpty()) {
             requestHeaderBuilder.append(line).append('\n');
             logger.debug("Header Line: {}", line);
         }
         return requestHeaderBuilder.toString();
     }
+
+    private String readBodyLine(BufferedReader httpRequest, int contentLength) throws IOException {
+        StringBuilder requestBodyBuilder = new StringBuilder();
+
+        char[] buffer = new char[contentLength];
+        int bytesRead = httpRequest.read(buffer, 0, contentLength);
+        requestBodyBuilder.append(buffer, 0, bytesRead);
+        return requestBodyBuilder.toString();
+    }
+
 
     public HttpRequestStartLine getHttpRequestStartLine() {
         return httpRequestStartLine;
