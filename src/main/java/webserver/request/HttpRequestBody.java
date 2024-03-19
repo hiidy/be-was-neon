3afaa9806b7cecc6239857f9d42b736f683fde9e
hiidy <hiidy115@gmail.com>
2024-03-19 13:41:55 +0900

    POST 메서드를 위한 HttpRequestBody 클래스 구현

---
+++ src/main/java/webserver/request/HttpRequestBody.java
@@ -0,0 +1,11 @@
+package webserver.request;
+
+public class HttpRequestBody {
+
+    private String bodyMessage;
+
+    public HttpRequestBody(String bodyMessage) {
+        this.bodyMessage = bodyMessage;
+    }
+
+}
