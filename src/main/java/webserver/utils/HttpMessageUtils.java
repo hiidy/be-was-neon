e83f65ad1531e01a906e9f7331dc1708b1b7e6be
hiidy <hiidy115@gmail.com>
2024-03-19 15:50:54 +0900

    HttpMessageUtils로 클래스명 변경
    - 기존 클래스명이 클래스의 핵심을 잘 표현하지 못한다고 해서 이름 변경
    - query문을 파싱하는 기능을 HttpMessageUtils로 옮겼습니다.

--- src/main/java/webserver/utils/HttpMessageUtils.java
+++ src/main/java/webserver/utils/HttpMessageUtils.java
@@ -4,8 +4,11 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
 
-public class ReadingFiles {
+public class HttpMessageUtils {
 
     public static byte[] readByteFromFile(File file) {
         try (FileInputStream fileInputStream = new FileInputStream(file);) {
@@ -17,6 +20,21 @@ public class ReadingFiles {
             return byteArrayOutputStream.toByteArray();
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    public static void parseQueryString(String bodyMessage, Map<String, String> queryMap) {
+
+        String[] pairs = bodyMessage.split("&");
+
+        for (String pair : pairs) {
+
+            String[] keyValue = pair.split("=");
+
+            String key = keyValue[0];
+            String value = URLDecoder.decode(keyValue[1], StandardCharsets.UTF_8);
+
+            queryMap.put(key, value);
         }
     }
 
