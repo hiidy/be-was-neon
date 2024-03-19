3e6d26513569a419f08df216d7583ac184d17bdc
hiidy <hiidy115@gmail.com>
2024-03-19 13:40:43 +0900

    HttpRequestHeader를 linkedHashMap으로 구현 및 테스트

--- src/main/java/webserver/request/HttpRequestHeader.java
+++ src/main/java/webserver/request/HttpRequestHeader.java
@@ -1,10 +1,35 @@
 package webserver.request;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public class HttpRequestHeader {
 
-    private String headerMessage;
+    private static final String CRLF = "\r\n";
 
-    public HttpRequestHeader(String headerMessage) {
-        this.headerMessage = headerMessage;
+
+    private final Map<String, String> requestHeader;
+
+    public HttpRequestHeader(String headersInput) {
+        this.requestHeader = new HashMap<>();
+        parseHeader(headersInput);
     }
+
+    private void parseHeader(String headersInput) {
+        String[] headers = headersInput.split(CRLF);
+        for (String header : headers) {
+            String[] keyValue = header.split(":", 2);
+            requestHeader.put(keyValue[0].trim(), keyValue[1].trim());
+        }
+    }
+
+    public Map<String, String> getHeaders() {
+        return Collections.unmodifiableMap(requestHeader);
+    }
+
+    public String getValue(String key) {
+        return getHeaders().get(key);
+    }
+
 }
