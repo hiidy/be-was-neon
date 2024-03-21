b6cc5be88826654bc64ad40d2dc4df8732ea5245
hiidy <hiidy115@gmail.com>
2024-03-21 14:21:56 +0900

    feat(session): Session 클래스 추가
    - session cookie 기능 구현을 위해서 session 정보를 담는 클래스 구현

---
+++ src/main/java/webserver/session/Session.java
@@ -0,0 +1,12 @@
+package webserver.session;
+
+public class Session {
+
+    private final String sessionId;
+
+    public Session(String sessionId) {
+        this.sessionId = sessionId;
+    }
+
+
+}
