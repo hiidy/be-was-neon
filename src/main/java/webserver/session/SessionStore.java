c8e116aeb6fb464540e8f534cecab08bda6c0b46
hiidy <hiidy115@gmail.com>
2024-03-22 14:37:29 +0900

    feat: session을 삭제하는 메서드를 추가

--- src/main/java/webserver/session/SessionStore.java
+++ src/main/java/webserver/session/SessionStore.java
@@ -8,11 +8,15 @@ public class SessionStore {
 
     private static final Map<Session, String> sessions = new ConcurrentHashMap<>();
 
-    public void addSession(Session session, String userId) {
+    public static void addSession(Session session, String userId) {
         sessions.put(session, userId);
     }
 
     public Map<Session, String> getSessions() {
         return Collections.unmodifiableMap(sessions);
+    }
+
+    public static void removeSession(String sessionId) {
+        sessions.entrySet().removeIf(entry -> entry.getKey().getSessionId().equals(sessionId));
     }
 }
